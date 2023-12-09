3e437570c0f27c2a52ea904c6e241be153c8e947
Qiuguorong1997 <86104061+Qiuguorong1997@users.noreply.github.com>
2023-12-09 17:41:45 +0800

    去除贪吃蛇项目

--- pysnake-master/src/mygame/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-
-from mygame.mygame import MyGame
-from mygame.apple import Apple
-from mygame.snake import Snake
-from mygame.astar import Astar
-from mygame.settings import *
